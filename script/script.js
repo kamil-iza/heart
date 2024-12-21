259074bb3d1d8916976d6d82bac8541a5a922cff
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2024-12-21 11:41:10 +0100

    Add content

---
+++ script/script.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll("section");
+
+const revealSection = () => {
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight * 0.75) {
+      section.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealSection);
+
+// Initial reveal
+revealSection();
